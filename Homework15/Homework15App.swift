890921ed495695290e767e0b3139d0d326aee58e
David Deng <daviddeng@my.smccd.edu>
2025-11-26 02:50:59 -0800

    Initial Commit

---
+++ Homework15/Homework15App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework15App.swift
+//  Homework15
+//
+//  Created by David Deng on 11/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Homework15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
